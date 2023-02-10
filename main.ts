ddb5c9628c4f0a2877c0e02178b4cd190b3da2aa
Tubagusva <124652119+Tubagusva@users.noreply.github.com>
2023-02-10 14:37:40 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showIcon(IconNames.Asleep)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Asleep)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
